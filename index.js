ab8a1c3fcf64c4a0bdea9744034b13159a8fb970
Jawad <jawad@verysafenet.com>
2021-09-04 17:18:32 +0100

    second commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello World!\n'
+  const msg = 'Hello World!\nCeci est un test\n'
   res.end(msg);
 });
 
